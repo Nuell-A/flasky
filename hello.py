19f1d0b4c94baccd5bb3b6618ff78fbfb7caeea2
Nuell <alcala.arredondo1@gmail.com>
2022-06-26 02:30:40 +0100

    Installed bootstrap, created 404 & 500 error handler templates, implemented bootstrap into templates

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -11,3 +13,10 @@ def user(name):
     # 'format(name)' is inserted in place of the curly braces
     return render_template('user.html',  name=name)
     
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
